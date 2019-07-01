ef0a47a1d0b9e71a25996085209a59049293d05a
Michael Larsson <michael.larsson@tui.se>
2019-07-01 10:17:34 +0200

    WIP

--- src/navigation/index.js
+++ src/navigation/index.js
@@ -2,30 +2,38 @@ import React, { useState, useEffect } from 'react';
 import { View, StyleSheet, Dimensions, StatusBar } from 'react-native';
 import { TabView, SceneMap, TabBar, Icon } from 'react-native-tab-view'; 
 
-import FirstPage from '../tabs/first-page';
+import Home from '../tabs/home';
+import Profile from '../tabs/Profile';
+import Statistics from '../tabs/Statistics';
 
-const FirstRoute = () => (
-  <FirstPage />
+const Home = () => (
+  <Home />
 );
 
-const SecondRoute = () => (
-  <View style={[styles.scene, { backgroundColor: 'green' }]} />
+const Profile = () => (
+  <Profile />
+);
+
+const Statistics = () => (
+  <Statistics />
 );
 
 export default function Navigation() {
   const [ state, setState ] = useState({
     index: 0,
     routes: [
-      { key: 'first', title: 'Home' },
-      { key: 'second', title: 'Second' },
+      { key: 'home', title: 'Home' },
+      { key: 'profile', title: 'Profile' },
+      { key: 'statistics', title: 'Statistics' },
     ],
   });
   return (
     <TabView
         navigationState={ state }
         renderScene={ SceneMap({
-            first: FirstRoute,
-            second: SecondRoute,
+          home: Home,
+          profile: Profile,
+          statistics: Statistics,
         })}
         onIndexChange={ index => setState( {...state, index } )}
         initialLayout={{ width: Dimensions.get('window').width }}
